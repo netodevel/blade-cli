80fc63321c4ce84a88f363865ac5f83a3823e44c
José Vieira <josevieira.dev@gmail.com>
2017-06-27 15:37:46 -0300

    add compile all classes in command run

--- run.rb
+++ run.rb
@@ -1,6 +1,12 @@
 class Run
 
-    def load_dependencies 
+    def init
+      compile_all_classes()
+      load_dependencies()
+    end
+
+    def load_dependencies
+        system('mvn dependency:resolve')
         system('mvn -o dependency:list -DoutputFile="list-dependencies.txt" -DoutputAbsoluteArtifactFilename=true')
         dependencies = ""
         File.open('list-dependencies.txt', 'r') do |f1|
@@ -11,13 +17,32 @@ class Run
                 end
             end
         end
+        puts dependencies
         path_build = Dir.pwd + "/target/classes"
         command = "java -Dfile.encoding=UTF-8 -classpath #{path_build}#{dependencies} com.example.myapp.Application"
         puts command
         system(command)
     end
-    
+
+    def compile_all_classes
+      system('mvn -o dependency:list -DoutputFile="list-dependencies.txt" -DoutputAbsoluteArtifactFilename=true')
+      dependencies = ""
+      File.open('list-dependencies.txt', 'r') do |f1|
+          while line = f1.gets
+              path_dependency = line.split(':/')
+              unless (path_dependency[1].to_s.empty?)
+                  dependencies +=  ":/" + path_dependency[1].strip
+              end
+          end
+      end
+      puts dependencies
+      path_build = Dir.pwd + "/target/classes"
+      command = "javac -d #{path_build} -cp #{dependencies} src/main/java/com/example/myapp/*.java"
+      puts command
+      system(command)
+    end
+
 end
 
 run = Run.new
-run.load_dependencies
+run.init
